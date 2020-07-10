cdf3d303eeb5def812280c0d29920f0dfb51a114
fodw1 <39199713+fodw1@users.noreply.github.com>
2020-07-09 22:08:55 -0300

    Update Alert Boss Wartale.js
    I change somethings and now running with no erros but need some things....

--- Alert Boss Wartale.js
+++ Alert Boss Wartale.js
@@ -2,48 +2,61 @@ registerPlugin({
     name: 'Alert Boss Wartale',
     version: '1.0',
     backends: ['ts3'],
-    description: 'This plugin alerts all bosses that will be born 15 minutes earlier.',
+    description: 'This plugin alerts all bosses will be spawned 15 minutes earlier.',
     author: 'And4e - Wartale <andre3397@protonmail.com>',
-    vars: [
-      {
-        name: 'groupstoadd',
-        title: 'Groups for alert about boss add here, Group ID (ServerGroupID\'s)', // grupos que vão ser adicionados para receber o alerta do boss
-        type: 'string',
+    vars: {
+        groupstoreceived: { // Grupos que vão ser adicionados para receber o alerta do boss
+        title: 'Groups for alert about boss add here',
+        type: 'strings',
       },
-      {
-        name: 'bosstimeserver',
-        title: 'Add here time of server example: 01:50', // tempo do boss de acordo com o servidor (precisa ser trocado diariamente)
-        type: 'string',
+        bosstimeserver: { // Tempo do boss de acordo com o servidor (precisa ser trocado diariamente)
+        title: 'Add here 00~55',
+        placeholder: 'Default: 00~55',
+		type: 'number',
       },
-      { 
-        name: 'timetoalert',
-        title: 'Select the time to alert before the boss born.', // tempo de alerta conforme o boss time a escolha
+        timetoalert: { // Tempo de alerta conforme o boss time a escolha
+        title: 'Select the time to alert before the bosses spawned. Example: 01:50',
         type: 'select',
-        options: ['5 minutes before',
-                 '10 minutes before',
-                 '15 minutes before']
+        options: ['5',
+                 '10',
+                 '15']
       },
-      {
-          name:'messagetouser', //Message to user according with saved string
-          title:'Esreva uma mensagem que sera enviada (%t = tempo do servidor agora) ',
-          type:'string',
-      }
-
-],
-    autorun: false
+        nameallboss: {
+        title: 'Here all boss, select All boss for get all boss spawed on some time',
+        type: 'select',
+        options: ['All boss',
+		          'Draxos',
+		          'Centlon',
+				  'Guardião das Trevas',
+				  'Valento',
+				  'Fúria',
+				  'Anjo Caido',
+				  'Tulla',
+				  'Kelvezu',
+				  'Greedy',
+    ]
 },
-    (_, { message, type }) => {
-    const event = require('event')
-    var event = require('ClientServerGroupEvent')
-    
-    var groupstoadd = store.get('groupstoadd'); // deacordo com a docs estou obtendo as strings
-    var bosstimeserver = store.get('bosstimeserver'); // deacordo com a docs estou obtendo as strings
-    var timetoalert = store.get('timetoalert'); // deacordo com a docs estou obtendo as strings
-    
-    event.on('clientMove', ({ client, fromChannel }) => {
-        let msg = message.replace('%t', client.name())
-        if (!fromChannel) {
-            if (ClientServerGroupEvent, id():groupstoadd) {
-                client.chat(msg)
-            }
-});
+		timezone: {
+        title: 'Time zone',
+        type: 'select',
+        options: [
+				'UTC-04:00',
+				'UTC-03:30',
+				'UTC-03:00',
+         ]
+		}
+	}
+}, function(sinusbot, config) {
+
+    var event = require('event');
+    var engine = require('engine');
+    var backend = require('backend');
+//  var store = require('store');
+	
+//	var config.clientInfo = backend.getServerGroupByID();
+	if (config.backend.getServerGroupByID(!config.groupstoreceived)) {
+	event.on('backend.chat') (	
+                             msg('Ola, ' + config.nameallboss) )
+	engine.log("If not configureted Alert Boss Wartale, please disable it.");
+	                          }
+})
